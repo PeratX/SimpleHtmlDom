61bda48d07afe29b4eae8126f559dd66cbfaecae
PeratX <1215714524@qq.com>
2017-03-10 23:03:30 +0800

    Typo

--- src/PeratX/SimpleHtmlDom/SimpleHtmlDomNode.php
+++ src/PeratX/SimpleHtmlDom/SimpleHtmlDomNode.php
@@ -8,8 +8,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author iTXTech
- * @website https://itxtech.org
+ * @author PeratX
  */
 
 /**
